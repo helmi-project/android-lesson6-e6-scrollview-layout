024b6fe24ad61c0f506ff65e72db40a620ba22db
helmibelogix <helmi@belogix.com>
2020-11-02 11:32:36 +0700

    First Commit

---
+++ app/src/main/java/com/androidatc/lesson06_e6_scrollviewlayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.androidatc.lesson06_e6_scrollviewlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
